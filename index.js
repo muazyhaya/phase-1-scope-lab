01dc16462c9ef24380d1e98d608ce4ba95979a18
Muaz Yhaya <muazyhaya@outlook.com>
2021-07-22 14:07:09 -0700

    commit tree 76db44475a86207d47b14c30d270025fcddd568f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+ customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+BestCustomer;
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = "Karen"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Jen";
+}
+
+changeLeastFavoriteCustomer();
+
+
